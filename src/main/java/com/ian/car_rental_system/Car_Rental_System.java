a7fc31126b7f532d1a5406e4433e20d6c6c38473
iankristoper <iankristoper26@gmail.com>
2024-03-17 11:31:01 +0800

    UPDATE: main class features

---
+++ src/main/java/com/ian/car_rental_system/Car_Rental_System.java
@@ -0,0 +1,18 @@
+
+
+package com.ian.car_rental_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+
+public class Car_Rental_System 
+{
+
+    public static void main(String[] args) 
+    {
+        
+    }
+}
