a9e1c789f91fcaef09dcac57bc142ab4dc640a3d
iankristoper <iankristoper26@gmail.com>
2024-03-20 17:24:10 +0800

    UPDATE: car class features

--- src/main/java/com/ian/car_rental_system/Car.java
+++ src/main/java/com/ian/car_rental_system/Car.java
@@ -8,5 +8,92 @@ package com.ian.car_rental_system;
 
 public class Car 
 {
+    //private attirbutes for data hiding
+    private String make;
+    private String model;
+    private int year;
+    private double mileage;
+    private boolean available;
+    private String rentalHistory;
     
+    
+    //constructor methods for initiating initial values 
+    public Car(String make, String model, int year, double mileage)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.available = true;
+        this.rentalHistory = "";
+    }
+    
+    
+    
+    //getters for accessing the data inside
+    public String getMake() {
+        return make;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    
+    public double getMileage() {
+        return mileage;
+    }
+    
+    public boolean isAvailable() {
+        return available;
+    }
+    
+    public String getRentalHistory() {
+        return rentalHistory;
+    }
+    
+    
+    
+    //
+    public void rentCar()
+    {
+        if(available)
+        {
+            available = false;
+            rentalHistory = rentalHistory + "Rented";
+        }
+        else 
+        {
+            System.out.println("Car not available for renting");
+        }
+    }
+    
+    public void returnCar()
+    {
+        if(!available)
+        {
+            available = true;
+            rentalHistory = rentalHistory + "Returned";
+        }
+        else 
+        {
+            System.out.println("Car is already available");
+        }
+    }
+    
+    
+    @Override
+    public String toString() 
+    {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", mileage=" + mileage +
+                ", available=" + available +
+                '}';
+    }
 }
